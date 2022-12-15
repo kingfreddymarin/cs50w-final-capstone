d66b23bd888225f63dccc9a904205f2eecd28bff
Freddy Marin <freddymarin.jpg@gmail.com>
2022-12-14 23:57:26 -0600

    getting the current user's data

---
+++ frontend/src/pages/Test.jsx
@@ -0,0 +1,9 @@
+const Test = () => {
+    return (
+        <>
+            TEST
+        </>
+    );
+}
+
+export default Test;
--- frontend/src/router/App.js
+++ frontend/src/router/App.js
@@ -7,10 +7,12 @@ import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Redirect from "../pages/Redirect"
+import Test from "../pages/Test"
 
 function App() {
   const token = window.localStorage.getItem("token")
   const [isValid, setIsValid] = useState(null)
+  const [loggedUser, setLoggedUser] = useState({})
   useEffect(() => {
     axios
       .get("http://localhost:8000/user/", {
@@ -19,11 +21,14 @@ function App() {
         }
       }).then(function (response) {
         console.log(response);
+        const user = response.data.user_info
+        setLoggedUser(user)
         setIsValid(true)
       }).catch(function (error) {
         console.log(error);
         window.localStorage.removeItem("token")
         window.localStorage.removeItem("isLoggedIn")
+        setLoggedUser({})
         setIsValid(false)
       });
   }, [])
@@ -32,6 +37,8 @@ function App() {
       <Routes>
         <Route path="/" element={isValid ? <Layout /> : <Login />}>
           <Route index element={<Home />} />
+          <Route path="/test" element={<Test />} />
+
           {/* <Route path="*" element={<NoPage />} /> */}
         </Route>
         <Route path="/register" element={!isValid ? <Register /> : <Redirect />} />
