9b98fdbf2ecf36d4370285ee815f2adac2819848
Freddy Marin <freddymarin@pop-os.localdomain>
2023-01-10 15:26:21 -0600

    added profile styling

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,37 +1,48 @@
 // https://www.bootdey.com/snippets/view/profile-with-data-and-skills
+import { useEffect } from "react"
+import "../styles/Profile.css"
 const Profile = ({ currentUser }) => {
 
     const { username, email, first_name, last_name, likes, dislikes, comments, profile_data } = currentUser
     const { ctg_following } = profile_data
+    useEffect(() => {
+        console.log(ctg_following)
+    }, [currentUser])
     return (
         <>
-            <h1>Welcome back, {first_name} {last_name}!</h1>
-            <h3>{username}</h3>
-            <p>{email}</p>
-            <h5>Following: {ctg_following.length}</h5>
-
-            <br />
-            <h5>likes: {likes.length}</h5>
-            {likes.map((like) => {
-                const { timestamp } = like
-                return (
-                    <p key={like.id}>{timestamp}</p>
-                )
-            })}
-            <h5>comments: {comments.length}</h5>
-            {comments.map((comment) => {
-                const { timestamp } = comment
-                return (
-                    <p key={comment.id}>{timestamp}</p>
-                )
-            })}
-            <h5>dislikes: {dislikes.length}</h5>
-            {dislikes.map((dislike) => {
-                const { timestamp } = dislike
-                return (
-                    <p key={dislike.id}>{timestamp}</p>
-                )
-            })}
+            <div className="container mt-5 mb-5">
+                <div className="row no-gutters">
+                    <div className="col-md-4 col-lg-4"><img src="https://i.imgur.com/aCwpF7V.jpg" /></div>
+                    <div className="col-md-8 col-lg-8">
+                        <div className="d-flex flex-column">
+                            <div className="d-flex flex-row justify-content-between align-items-center p-5 bg-dark text-white">
+                                <h3 className="display-5">{first_name} {last_name}</h3>
+                            </div>
+                            <div className="p-3 bg-black text-white">
+                                <h6>{email}</h6>
+                            </div>
+                            <div className="d-flex flex-row text-white">
+                                <div className="p-4 bg-primary text-center skill-block">
+                                    <h4>{likes.length}</h4>
+                                    <h6>Likes</h6>
+                                </div>
+                                <div className="p-4 bg-success text-center skill-block">
+                                    <h4>{comments.length}</h4>
+                                    <h6>Comments</h6>
+                                </div>
+                                <div className="p-4 bg-warning text-center skill-block">
+                                    <h4>{dislikes.length}</h4>
+                                    <h6>Dislikes</h6>
+                                </div>
+                                <div className="p-4 bg-danger text-center skill-block">
+                                    <h4>{ctg_following.length}</h4>
+                                    <h6>Categories</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
