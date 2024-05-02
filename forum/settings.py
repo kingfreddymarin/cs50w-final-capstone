0295ddf72be49a0f1f19f4b9f16fe691bcd66556
Freddy Marin <freddymarin.jpg@gmail.com>
2024-05-02 16:40:02 -0600

    removed rds info and went back to sqlite

--- forum/settings.py
+++ forum/settings.py
@@ -85,21 +85,13 @@ AUTH_USER_MODEL = 'api.User'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 
     # This goes into vault
-    # pass for Rigo: pass_12345$$ , user: rigod
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db_forum',
-        'USER': 'mysuperuser',
-        'PASSWORD': '12345678', 
-        'HOST': 'dbforum.ch2a26wy6ogo.us-east-1.rds.amazonaws.com',
-        'PORT': '5432', 
-    }
+    # pass for Rigo: pass_12345$$ , user: rigo
 
 }
 
